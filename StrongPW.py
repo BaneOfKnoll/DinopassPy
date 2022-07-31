5cf05018c0947ab2203c80f8d6e5f7af39f52547
Tom K <tkraayenbrink4@gmail.com>
2022-07-31 21:07:41 +1000

    It works now

--- StrongPW.py
+++ StrongPW.py
@@ -1,1 +1,10 @@
-print("HelloWorld")
+import requests
+import clipboard
+
+strong_url= "http://www.dinopass.com/password/strong"
+
+strongPass = requests.get(strong_url)
+
+clipboard.copy(str(strongPass.text))
+
+print(str(strongPass.text))
