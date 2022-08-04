337144cefd0de57b66561d9f4cc5742d00fd5258
Tom Kraayenbrink <tkraayenbrink4@gmail.com>
2022-08-04 19:17:14 +1000

    Fixed a variable placement that annoyed me

--- StrongPW.py
+++ StrongPW.py
@@ -2,15 +2,15 @@ import requests
 import clipboard
 
 multiple = input("Do you want more than 1? (y/n)")
+strong_url= "http://www.dinopass.com/password/strong"
+
 
 if multiple == "y":
     amount = input("How many?")
     for x in range(int(amount)):
-        strong_url= "http://www.dinopass.com/password/strong"
         strongPass = requests.get(strong_url)
         print(str(strongPass.text))
 else:
-    strong_url= "http://www.dinopass.com/password/strong"
     strongPass = requests.get(strong_url)
     print(str(strongPass.text))
     clipboard.copy(str(strongPass.text))
